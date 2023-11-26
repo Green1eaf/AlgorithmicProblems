1c9eea07c903f1c1a5c51cbf0996a6fbe289518d
oleg smirnov <green1eaf@yandex.com>
2023-11-26 17:32:10 +0300

    gitignore

---
+++ src/leetcode/BinaryTreePostOrderTraversal.java
@@ -0,0 +1,44 @@
+package leetcode;
+
+//  https://leetcode.com/problems/binary-tree-postorder-traversal/
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryTreePostOrderTraversal {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        helper(root, ans);
+        return ans;
+    }
+
+    private static void helper(TreeNode node, List<Integer> ans) {
+        if (node == null) return;
+        if (node.left != null) {
+            helper(node.left, ans);
+        }
+        if (node.right != null) {
+            helper(node.right, ans);
+        }
+        ans.add(node.val);
+    }
+}
