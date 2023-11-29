b0da1653c013c784c16402cf3aecb61e5940c6a2
oleg smirnov <green1eaf@yandex.com>
2023-11-29 20:52:56 +0300

    add solution v2

--- src/leetcode/SingleNumber.java
+++ src/leetcode/SingleNumber.java
@@ -2,9 +2,6 @@ package leetcode;
 
 //  https://leetcode.com/problems/single-number/
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class SingleNumber {
     public static void main(String[] args) {
         System.out.println(singleNumber(new int[]{2, 2, 1}));
@@ -13,13 +10,10 @@ public class SingleNumber {
     }
 
     private static int singleNumber(int[] nums) {
-        Map<Integer, Boolean> map = new HashMap<>();
+        int result = 0;
         for (int i : nums) {
-            map.put(i, map.get(i) == null);
+            result ^= i;
         }
-        for (var val : map.entrySet()) {
-            if (val.getValue()) return val.getKey();
-        }
-        return 0;
+        return result;
     }
 }
