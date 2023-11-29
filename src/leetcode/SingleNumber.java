00400846610164f860013d7d217ce3025aac17d7
oleg smirnov <green1eaf@yandex.com>
2023-11-29 21:15:07 +0300

    add solution v3

--- src/leetcode/SingleNumber.java
+++ src/leetcode/SingleNumber.java
@@ -2,6 +2,9 @@ package leetcode;
 
 //  https://leetcode.com/problems/single-number/
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class SingleNumber {
     public static void main(String[] args) {
         System.out.println(singleNumber(new int[]{2, 2, 1}));
@@ -11,8 +14,15 @@ public class SingleNumber {
 
     private static int singleNumber(int[] nums) {
         int result = 0;
+        Set<Integer> set = new HashSet<>();
         for (int i : nums) {
-            result ^= i;
+            if (set.contains(i)) {
+                result -= i;
+                set.remove(i);
+            } else {
+                result += i;
+                set.add(i);
+            }
         }
         return result;
     }
