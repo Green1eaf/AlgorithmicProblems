aa3d0ef6def96a7fc72fb0cda7ccab7ba6e5106d
oleg smirnov <green1eaf@yandex.com>
2023-11-29 20:12:51 +0300

    add solution

---
+++ src/leetcode/PascalsTriangle.java
@@ -0,0 +1,32 @@
+package leetcode;
+
+//  https://leetcode.com/problems/pascals-triangle/
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class PascalsTriangle {
+    public static void main(String[] args) {
+        System.out.println(generate(5));
+    }
+
+    private static List<List<Integer>> generate(int numRows) {
+        if (numRows == 0) return Collections.emptyList();
+        if (numRows == 1) return List.of(List.of(1));
+        List<List<Integer>> ans = new ArrayList<>(List.of(List.of(1)));
+        for (int i = 1; i < numRows; i++) {
+            ans.add(helper(ans.get(i - 1)));
+        }
+        return ans;
+    }
+
+    private static List<Integer> helper(List<Integer> list) {
+        List<Integer> result = new ArrayList<>(List.of(1));
+        for (int i = 1; i < list.size(); i++) {
+            result.add(list.get(i - 1) + list.get(i));
+        }
+        result.add(1);
+        return result;
+    }
+}
