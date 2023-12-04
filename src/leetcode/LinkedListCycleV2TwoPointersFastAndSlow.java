fae7aa220e0dd3647a293323c757f76f30bb1498
oleg smirnov <green1eaf@yandex.com>
2023-12-04 17:56:02 +0300

    add solution

---
+++ src/leetcode/LinkedListCycleV2TwoPointersFastAndSlow.java
@@ -0,0 +1,26 @@
+package leetcode;
+
+//  https://leetcode.com/problems/linked-list-cycle/
+
+public class LinkedListCycleV2TwoPointersFastAndSlow {
+    class ListNode {
+        int val;
+        LinkedListCycle.ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public static boolean hasCycle(LinkedListCycle.ListNode head) {
+        var slow = head;
+        var fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) return true;
+        }
+        return false;
+    }
+}
