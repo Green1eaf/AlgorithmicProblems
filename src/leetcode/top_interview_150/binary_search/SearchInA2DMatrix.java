8fceb0300a4d6a9323dda64462abac841e3e26b1
oleg smirnov <green1eaf@yandex.com>
2023-12-13 15:10:12 +0300

    add solution

---
+++ src/leetcode/top_interview_150/binary_search/SearchInA2DMatrix.java
@@ -0,0 +1,22 @@
+package leetcode.top_interview_150.binary_search;
+
+//  https://leetcode.com/problems/search-a-2d-matrix
+
+public class SearchInA2DMatrix {
+    public static void main(String[] args) {
+        System.out.println(searchMatrix(new int[][]{{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 60}}, 13));
+    }
+
+    public static boolean searchMatrix(int[][] matrix, int target) {
+        int row = matrix.length;
+        int col = matrix[0].length;
+        int i = 0;
+        int j = col - 1;
+        while (i >= 0 && i < row && j >= 0 && j < col) {
+            if (matrix[i][j] == target) return true;
+            if (matrix[i][j] > target) j--;
+            else i++;
+        }
+        return false;
+    }
+}
