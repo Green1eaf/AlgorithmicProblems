0ecc6b11f6a07d24dc89523a861969f283fa86b9
oleg smirnov <green1eaf@yandex.com>
2023-12-17 18:35:09 +0300

    add solution

---
+++ src/leetcode/top_interview_150/array_string/LengthOfLastWord.java
@@ -0,0 +1,19 @@
+package leetcode.top_interview_150.array_string;
+
+public class LengthOfLastWord {
+    public static void main(String[] args) {
+        System.out.println(lengthOfLastWord("Hello World"));    //5
+        System.out.println(lengthOfLastWord("   fly me   to   the moon  "));    //4
+        System.out.println(lengthOfLastWord("luffy is still joyboy"));  //6
+    }
+
+    public static int lengthOfLastWord(String s) {
+        int ans = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (Character.isLetter(s.charAt(i))) {
+                ans++;
+            } else if (ans != 0) break;
+        }
+        return ans;
+    }
+}
